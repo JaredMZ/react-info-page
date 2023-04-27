96019215d348b9bef1811fdfb6c9c5db5fb02d7b
JaredMZ <jaredcv04@gmail.com>
2023-04-27 15:22:23 -0600

    initial commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <img src="/src/assets/react.svg" width="30px" className="navbar--logo" ></img>
+            <h3 className="navbar--logo_text">ReactFacts</h3>
+            <h4 className="navbar--text">React Course - Project 1</h4>
+        </nav>
+    )
+}
+
+
